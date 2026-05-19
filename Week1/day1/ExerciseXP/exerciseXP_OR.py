53ed2bdb949731745c2f5ee882006a83598a1adf
BlackCyrus2002 <atchue2002@gmail.com>
2026-05-19 23:06:21 +0000

    exercicexp_or

--- Week1/day1/ExerciseXP/exerciseXP_OR.py
+++ Week1/day1/ExerciseXP/exerciseXP_OR.py
@@ -1,58 +1,58 @@
 # Exercice 1
 
-# mois = input("Veuillez choisir un moi entre 1 et 12 : ")
-# mois = int(mois)
+mois = input("Veuillez choisir un moi entre 1 et 12 : ")
+mois = int(mois)
 
-# if mois >=3 and mois <=5 :
-#     print("Spring")
-# elif mois >=6 and mois <=8 :
-#     print("Summer")
-# elif mois >= 9 and mois <= 11 :
-#     print("Autumn")
-# elif mois == 12 or mois == 1 or  mois ==2 :
-#     print("Winter")
-# else :
-#     print(f"Le mois {mois} n'existe pas")
+if mois >=3 and mois <=5 :
+    print("Spring")
+elif mois >=6 and mois <=8 :
+    print("Summer")
+elif mois >= 9 and mois <= 11 :
+    print("Autumn")
+elif mois == 12 or mois == 1 or  mois ==2 :
+    print("Winter")
+else :
+    print(f"Le mois {mois} n'existe pas")
 
 # Exercice 2
-# nombre = range(1,21)
-# for nb in nombre:
-#     print(nb)
+nombre = range(1,21)
+for nb in nombre:
+    print(nb)
 
-# for nb in nombre:
-#     if (nb % 2 ==0) :
-#         print(nb)
+for nb in nombre:
+    if (nb % 2 ==0) :
+        print(nb)
 
 # Exercice 3
-# nom = "Assan"
+nom = "Assan"
 
-# nom_utilisateur = input("Devinez mon nom : ")
+nom_utilisateur = input("Devinez mon nom : ")
 
-# while nom_utilisateur.lower() != nom.lower() :
-#     nom_utilisateur = input("Mauvais nom, essayez encore : ")
+while nom_utilisateur.lower() != nom.lower() :
+    nom_utilisateur = input("Mauvais nom, essayez encore : ")
 
 # Exerice 4
 
-# names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
 
-# name_user = input("Entrez votre nom : ")
+name_user = input("Entrez votre nom : ")
 
-# for name in names :
-#     if name == name_user :
-#         print(f"L'index appartenant au nom est {names.index(name)}")
-#         break;
+for name in names :
+    if name == name_user :
+        print(f"L'index appartenant au nom est {names.index(name)}")
+        break;
 
 # Exerice 5
 total_nombre = []
-# nombre1 = input("Entrez le premier nombre : ")
-# nombre2 = input("Entrez le premier nombre : ")
-# nombre3 = input("Entrez le premier nombre : ")
+nombre1 = input("Entrez le premier nombre : ")
+nombre2 = input("Entrez le premier nombre : ")
+nombre3 = input("Entrez le premier nombre : ")
 
-# total_nombre.append(int(nombre1))
-# total_nombre.append(int(nombre2))
-# total_nombre.append(int(nombre3))
+total_nombre.append(int(nombre1))
+total_nombre.append(int(nombre2))
+total_nombre.append(int(nombre3))
 
-# print(f"Le plus grand est {max(total_nombre)}")
+print(f"Le plus grand est {max(total_nombre)}")
 
 #Exerice 6
 import random
