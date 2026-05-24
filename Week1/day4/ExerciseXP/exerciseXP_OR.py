d650e4d5a9332bcc8f09ce3049b75489e8d5122d
BlackCyrus2002 <atchue2002@gmail.com>
2026-05-24 17:36:04 +0000

    XP or day 4

--- Week1/day4/ExerciseXP/exerciseXP_OR.py
+++ Week1/day4/ExerciseXP/exerciseXP_OR.py
@@ -1,97 +1,103 @@
-# Exerice 1
-# class Circle :
-#     def __init__(self,radius):
-#         self.radius = radius
-#         self.perimetre_cercle = 0
-#         self.aire_cercle = 0
-    
-#     def perimetre (self):
-#         self.perimetre_cercle = 2*self.radius*3.141
-#         print(f"Le périmètre du cercle est {self.perimetre_cercle}")
-        
-    
-#     def aire(self) :
-#         self.aire_cercle = 3.141*self.radius**2
-#         print(f"L'aire du cercle est {self.aire_cercle}") 
-    
-#     def affiche(self):
-#         print(f"On a un cercle de rayon de {self.radius} où l'aire est {self.aire_cercle} et le périmètre est {self.perimetre_cercle}")
+#Partie I
+class BankAccount :
+    def __init__(self, balance,username, password):
+        self.balance = balance
+        self.username = username
+        self.password = password
+        self.authenticated = False
 
-# circle = Circle(2)
-# circle.perimetre()
-# circle.aire()
-# circle.affiche()
-
-# Exercice 2
-# import random
-# class MyList :
-#     def __init__(self,letter):
-#         self.letter = letter
-#         self.new_list = []
-    
-#     def inverse(self):
-#         print(f"La liste inversé est {reversed(self.letter)}")
-    
-#     def ranger(self):
-#         print(f"La nouvelle liste triée est {sorted(self.letter)}")
-    
-#     def newList(self):
-#         for i in range(0,len(self.letter)):
-#             self.new_list.append(random.randint(0,100))
-        
-#         print(f"La liste est : {self.new_list}")
-
-# mylist = MyList(["Cyriac","Assan","test"])
-# mylist.inverse()
-# mylist.ranger()
-# mylist.newList()
-
-#Exerice 3
-class MenuManager :
-    def __init__(self,menu):
-        self.menu = menu
-    
-    def add_item(self,name, price, spice, gluten) :
-        plat = {
-            "name" : name,
-            "price" : price,
-            "spice" : spice,
-            "gluten" : gluten
-        }
-
-        self.menu.append(plat)
-
-    def update_item(self,name, price, spice, gluten) :
-        for m in self.menu :
-            if m["name"] == name :
-                m['price'] = price
-                m['spice'] = spice
-                m['gluten'] = gluten
-                print(f"Menu {name} mis à jour")
-
-
-    def remove_item(self,name) :
-        self.menu = [m for m in self.menu if m['name'] !=name ]
-
-
-    def afficher(self) :
-        print(f"La liste des menus est : ")
-        for menu in self.menu :
-            print("************************")
-            print(f"Titre : {menu["name"]}")
-            print(f"Prix : {menu["price"]} FCFA")
-            print(f"Niveau d'épice : {menu["spice"]}")
-            if menu["gluten"] ==True:
-                print("Glutenn : Avec gluten")
+    def deposit(self,money):
+        if self.authenticated :
+            if money > 0:
+                self.balance += money
+                return self.balance
             else :
-                print('Gluten : Sans gluten')
+                raise Exception("The amount seized is less than 1 FCFA")
+        else :
+            raise Exception ('You are not connected')
+    
+    def withdraw (self,money):
+        if(self.authenticated):
+            if money > 0:
+                if money <= self.balance :
+                    self.balance -= money
+                else :
+                    raise Exception("Insufficient funds to make a withdrawal !")
+            else :
+                raise Exception("The amount seized is less than 1 FCFA")
+        else :
+            raise Exception ("You are not connected")
+    
+    def authenticate(self,username, password) :
+        if username == self.username and password == self.password :
+            self.authenticated = True
+            print('You are connected !')
+        else :
+            raise Exception ('Username or password is incorrect')
 
 
-menu_manager = MenuManager([])
-menu_manager.add_item("garba",500,"B", True)
-menu_manager.add_item("tchep",1000,"A", False)
-menu_manager.afficher()
-menu_manager.remove_item('garba')
-menu_manager.afficher()
-menu_manager.update_item("tchep",5000,"A", False)
-menu_manager.afficher()
+class MinimumBalanceAccount(BankAccount) :
+    def __init__(self, balance,username, password, minimum_balance = 0):
+        super().__init__(balance,username, password)
+        self.minimum_balance = minimum_balance
+    
+    def withdraw(self, money):
+        if self.authenticated :
+            if money > 0:
+                #Vérification funds
+                if self.balance - money >= self.minimum_balance :
+                    self.balance -= money
+                    print(self.balance)
+                else :
+                    raise Exception("Insufficient funds to make a withdrawal !")
+            else :
+                raise Exception("The amount seized is less than 1 FCFA")
+        else :
+            raise Exception ('You are not connected')
+
+
+# class ATM :
+#     def __init__(self, account_list,try_limit):
+#         self.account_list = account_list
+#         self.try_limit = try_limit
+    
+#     def log_in(self):
+#         print("DO YOU WANT TO CONNECTED ? ENTER YOU INFORMATIONS")
+#         name = input("Name : ")
+#         password = input("Password : ")
+
+#         for account in self.account_list :
+#             if MinimumBalanceAccount.authenticate(name,password) :
+
+
+
+#     def show_main_menu(self) :
+#         print("*************** DO YOU WANT TO DO OPERATION ? : *****************")
+#         print('1 - Connexion \n2 - Exit')
+#         user_choice = 0
+#         while True :
+#             choice = input("Select : ")
+#             if choice == "1" or choice == "2" :
+#                 user_choice = choice
+#                 break
+        
+#         if user_choice == "2" :
+#             return print('Thank\'s, see you soon !')
+#         else :
+#             self.log_in()
+            
+        
+
+
+account = MinimumBalanceAccount(
+    100,
+    "Cyriac79",
+    "azerty",
+    minimum_balance=20
+)
+
+account.authenticate("Cyriac79", "azerty")
+
+account.deposit(50)
+
+account.withdraw(110)
